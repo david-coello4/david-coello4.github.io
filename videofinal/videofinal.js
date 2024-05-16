65219501838e9c29b73f4e9e677021cde16bc259
david-coello4 <davidecoello2@gmail.com>
2024-05-16 01:59:58 -0700

    replaced

--- videofinal/videofinal.js
+++ videofinal/videofinal.js
@@ -11,5 +11,15 @@ function setup() {
   // Create a <p> element to display text
   let textElement = createP('Click the video for a fun video');
   textElement.style('font-size', '24px'); // Adjust font size
-  textElement.style('color', 0); // Adjust text color
+  textElement.style('color', '#000000'); // Adjust text color
+
+  // Create the blank button
+  let blankButton = createImg("https://david-coello4.github.io/videofinal/disk.gif");
+  blankButton.position(325, 715);
+  blankButton.size(90, 90); // Adjust button size here
+  blankButton.mousePressed(openBlank); // Call openBlank function when clicked
+}
+
+function openBlank() {
+  window.open('https://david-coello4.github.io/artfinal/index.html', '_blank');
 }
